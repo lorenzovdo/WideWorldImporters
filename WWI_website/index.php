89bf85e6db96f5144f7e64b66ee722c3480ef409
MonkeyOS <MonkeyOS@users.noreply.github.com>
2018-11-02 15:08:41 +0100

    Add files via upload

--- WWI_website/index.php
+++ WWI_website/index.php
@@ -9,5 +9,52 @@ and open the template in the editor.
         <?php
         include 'Header.php';
         ?>
+        <div class="row justify-content-md-center" style="margin: 0; margin-top: 3%;">
+            <div class="col-md-2" style="padding-right: 2%;">
+                <div style="background-color:#EEEEEE; border: 1px solid black;">
+                    <h3 style="text-align:center;">Categorieën</h3>
+                    <ul>
+                        <li>Filter functie</li>
+                        <li>alle stuff</li>
+                        <li>waar mensen</li>
+                        <li>op willen filteren</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-7"><!--style="background-color:#AAAAAA"-->
+                <div class="row">
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4" style="height: 300px; padding: 1%; padding-top: 0;">
+                        <div style="background-color:#555555; width: 100%; height:100%;">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
